4709073568254f824b3d896181d92cc2a7484505
Дмитрий Типсин <tipsin.dm@yandex.ru>
2024-04-18 18:28:17 +0300

    ref: Удаление неиспользуемого параметра req из GET запроса

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { reviewsMock } from '@shared/mock/reviewsMock';
 
 
-const GET = async (req: Request) => {
+const GET = async () => {
   return NextResponse.json(reviewsMock);
 };
 
